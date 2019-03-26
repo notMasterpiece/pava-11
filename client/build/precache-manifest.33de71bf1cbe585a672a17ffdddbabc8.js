96094a4c9be3daf1dcc845e9300da9e90882101a
Administrator <administrator@iMac.local>
2019-03-26 13:22:44 +0200

    fix

---
+++ client/build/precache-manifest.33de71bf1cbe585a672a17ffdddbabc8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5de3e19f3a6aaaae35f55b04ba7ab2a3",
+    "url": "/static/media/bg2.5de3e19f.jpg"
+  },
+  {
+    "revision": "3600a323680bfdaa2fd5",
+    "url": "/static/css/main.fa2426b8.chunk.css"
+  },
+  {
+    "revision": "c0c8f32bf323b91cd0c72c06e383e9db",
+    "url": "/static/media/comment.c0c8f32b.svg"
+  },
+  {
+    "revision": "754cdda3e542af06a52b",
+    "url": "/static/js/1.754cdda3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a20bc2b68b9745f96c1a642463c3b1f",
+    "url": "/static/media/matrix.0a20bc2b.gif"
+  },
+  {
+    "revision": "3600a323680bfdaa2fd5",
+    "url": "/static/js/main.3600a323.chunk.js"
+  },
+  {
+    "revision": "7dbf4d49e885e6f009609e57bbc0bbc2",
+    "url": "/static/media/node2.7dbf4d49.svg"
+  },
+  {
+    "revision": "e85103d05c0161ea62895ae9fc4276fc",
+    "url": "/static/media/react1.e85103d0.svg"
+  },
+  {
+    "revision": "ed57793547cb030c8900300bb05b7665",
+    "url": "/static/media/logo.ed577935.svg"
+  },
+  {
+    "revision": "754cdda3e542af06a52b",
+    "url": "/static/css/1.4dcb0ee3.chunk.css"
+  },
+  {
+    "revision": "366311cf41e657d3b38aea5accdf739d",
+    "url": "/index.html"
+  }
+];
