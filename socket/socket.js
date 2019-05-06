c6e2ccac598fd42aebd4e84b58190b21ed90bcee
Administrator <administrator@iMac.local>
2019-05-06 17:33:36 +0300

    pull

--- socket/socket.js
+++ socket/socket.js
@@ -1,88 +1,227 @@
+const User = require('../models/Users');
+const Profile = require('../models/Profile');
+const ChatRoom = require('../models/ChatRoom');
 const Message = require('../models/Message');
-const Room = require('../models/ChatRoom');
-const passport = require('passport');
+const PMessage = require('../models/PrivateMessage');
+
+let globalRoom;
+
 
 module.exports = io => {
-    io.on('connect', function (socket) {
-        socket.emit('connected', 'You are connected to socket.io');
+    io.on('connection', socket => {
+        console.log('User connected');
 
-        socket.on('getAllMessages', () => {
-            Message
-                .find({})
-                .limit(30)
-                .sort({$natural: -1})
-                .sort({_id: -1})
-                .then(messages => {
+        socket.on('USER_CONNECT', user => {
+            console.log('CONNECT USER BY ID', user);
 
-                    messages.sort((a, b) => {
-                        return new Date((a.date)) - new Date((b.date));
-                    });
-
-                    io.emit('history', messages);
+            User.findByIdAndUpdate(user,
+                {
+                    $set: { online: true }
                 })
-                .catch(err => {
-                    console.log(err);
-                })
-        }),
+                    .then(() => {})
+                    .catch( err => console.log(err))
+        });
 
 
-        socket.on('msg', msg => {
+        socket.on('JOIN_ROOM', (room, callback) => {
 
-            const newMessage = new Message({
-                message: msg.message,
-            });
+            socket.join(room);
+            console.log(`user join to room ${room}`);
 
-            newMessage.save()
-                .then(() => {
-                    io.emit('message', newMessage);
-                })
-                .catch(err => {
-                    console.log(err);
+            Profile
+                .find()
+                .populate('user', ['avatar', 'name', '_id', 'online'])
+                .select('_id user')
+                .then(profiles => {
+                    profiles.sort((a, b) => b.user.online - a.user.online);
+                    io.to(room).emit('SERVER_ALL_USERS', profiles);
                 });
 
-        })
 
+            callback();
+        });
 
+        socket.on('NEW_CHAT_MSG', message => {
 
-
-        // small chat
-        socket.on('GET_ALL_MESSAGES', data => {
-            const {_id} = data;
-
-            Message
-                .find({room: _id})
-                .then(messages => {
-                    socket.emit('SET_ALL_MESSAGES', messages)
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-
-
+            // console.log(message);
+            io.to(message.room).emit('SERVER_NEW_CHAT_MSG', message);
 
         });
 
-        socket.on('add-message', data => {
-            const {message, room, user} = data;
 
-            const newMessage = new Message({
-                message,
-                room,
-                user
-            });
 
-            // console.log(newMessage);
+        socket.on('disconnect', socket => {
+            console.log('DISCONNECT', socket.id);
+            // set current user online : false
+        });
 
-            newMessage.save()
-                .then(res => {
-                    io.emit('added-message', res);
+
+
+
+
+
+
+        // private 15.04.2019
+
+        socket.on('PRIVATE_CONNECT', users => {
+
+            const {myId, userId} = users;
+
+            // console.log(myId, 'myID');
+            // console.log(userId, 'userID');
+
+
+
+            ChatRoom
+                .findOne({ users: { $all: [ myId , userId ] } })
+                .then(room => {
+                    if( room ) {
+
+                        globalRoom = room;
+                        socket.join(globalRoom._id);
+
+                        // console.log(`room found ${globalRoom._id}`);
+
+                        User
+                            .findById(userId)
+                            .then(user => {
+                                if(!user) return;
+                                // io.to(globalRoom._id).emit('SET_PRIVATE_USER_INFO', user);
+                                io.to(`${socket.id}`).emit('SET_PRIVATE_USER_INFO', user);
+                            });
+
+                        PMessage
+                            .find({room: globalRoom._id})
+                            .populate('user', ['avatar'])
+                            .select('message user icon createdAt _id')
+                            .sort('createdAt')
+                            .then(messages => {
+
+                                if( !messages.length ) return io.to(globalRoom._id).emit('SET_PRIVATE_ROOM_NO_MESSAGES');
+
+                                io.to(globalRoom._id).emit('SET_FIRST_MESSAGES', messages);
+                            })
+                            .catch(err => {
+                                console.log(err);
+                            })
+
+
+                    } else {
+
+
+                        // console.log('create new room');
+
+                        const newChatRoom = ChatRoom({
+                            name: 'New Room',
+                            users: [ myId , userId ],
+                            private: true
+                        });
+
+                        newChatRoom
+                            .save()
+                            .then(room => {
+
+                                globalRoom = room;
+                                socket.join(globalRoom._id);
+
+                                User
+                                    .findById(userId)
+                                    .then(user => {
+                                        if(!user) return;
+
+                                        // io.to(globalRoom._id).emit('SET_PRIVATE_USER_INFO', user);
+                                        io.to(`${socket.id}`).emit('SET_PRIVATE_USER_INFO', user);
+                                    });
+
+                                PMessage
+                                    .find({room: globalRoom._id})
+                                    .populate('user', ['avatar'])
+                                    .select('message user icon createdAt _id')
+                                    .then(messages => {
+
+                                        if( !messages.length ) return io.to(globalRoom._id).emit('SET_PRIVATE_ROOM_NO_MESSAGES');
+
+                                        io.to(globalRoom._id).emit('SET_FIRST_MESSAGES', messages);
+                                    })
+                                    .catch(err => {
+                                        console.log(err);
+                                    })
+
+
+                            })
+
+                    }
                 })
                 .catch(err => {
                     console.log(err);
                 });
+        });
 
-        })
+
+        socket.on('NEW_PRIVATE_MESSAGE', data => {
+
+            const newPrivateMessage = new PMessage({
+                message: data.message,
+                user: data.user,
+                room: globalRoom._id
+            });
+
+            newPrivateMessage
+                .save()
+                .then(message => {
+                    PMessage
+                        .findById(message._id)
+                        .populate('user', ['avatar'])
+                        .select('message user icon createdAt _id')
+                        // .exec()
+                        .then(message => {
+                            // console.log(messages);
+                            io.to(globalRoom._id).emit('SET_PRIVATE_MESSAGE', message);
+                        })
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        });
+
+
+
+        socket.on('TYPING', () => {
+            socket.broadcast.emit('SERVER_TYPING');
+        });
+
+
+
+        socket.on('ADD_IMAGE', data => {
+
+            const newPrivateMessage = new PMessage({
+                message: '',
+                icon: data.icon,
+                user: data.user,
+                room: globalRoom._id
+            });
+
+            newPrivateMessage
+                .save()
+                .then(message => {
+                    PMessage
+                        .findById(message._id)
+                        .populate('user', ['avatar'])
+                        .select('message user icon createdAt _id')
+                        // .exec()
+                        .then(message => {
+                            // console.log(message, 'icon');
+                            io.to(globalRoom._id).emit('SET_PRIVATE_MESSAGE', message);
+                        })
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+
+        });
+
 
 
     });
+
 };
